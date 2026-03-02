1ba68f713b190f0542ca52b7d8c97bc73435643b
ariannedee <ariannedee@gmail.com>
2026-03-02 12:04:58 -0800

    Add reading and pprint

--- examples/example_7_json.py
+++ examples/example_7_json.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from pprint import pprint
 
 with open("data/input.csv") as file:
     reader = csv.DictReader(file)
@@ -10,6 +11,17 @@ with open("data/output.json", "w") as file:
     json.dump(data, file)        # Write data directly to a file
 
 
+with open("data/output.json") as file:
+    data = json.load(file)       # Read data directly from a file
+    pprint(data)                 # pprint (pretty-print) adds newlines and indentation
+
+
 with open("data/output.json", "w") as file:
     json_str = json.dumps(data)  # Convert data to str
     file.write(json_str)         # Write str to file
+
+
+with open("data/output.json") as file:
+    contents = file.read()       # Read data as str
+    data = json.loads(contents)  # Convert str to objects
+    pprint(data)                 # Pretty print data
