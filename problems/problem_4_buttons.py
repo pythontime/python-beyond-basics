0d06f99b728a7bbeb1e88befbb4a82ce05571c23
ariannedee <ariannedee@gmail.com>
2026-03-02 15:42:23 -0800

    Update problem 4

--- problems/problem_4_buttons.py
+++ problems/problem_4_buttons.py
@@ -1,3 +1,9 @@
 """
 Print the text of all buttons on the Google homepage
 """
+from bs4 import BeautifulSoup
+
+with open('data/google.html', 'r') as file:
+    html_doc = file.read()
+
+print(html_doc)
